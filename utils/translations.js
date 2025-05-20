7e0d5f93ffc1eadfa284073ac6765ed1ca204032
sincere-abayo <abayosincere11@gmail.com>
2025-05-20 11:06:52 +0200

    s

--- utils/translations.js
+++ utils/translations.js
@@ -2,7 +2,7 @@ const translations = {
     en: {
       welcome: "Welcome to Rwanda Services\n1. Find Services\n2. Contact Us\n3. Change Language to Kinyarwanda",
       findServices: "Select a service:\n1. Healthcare\n2. Education\n3. Transportation\n4. Back to Main Menu",
-      contactUs: "Contact us:\nPhone: +250 XXX XXX XXX\nEmail: info@example.rw\n1. Back to Main Menu",
+      contactUs: "Contact us:\nPhone: 0786729283\nEmail: abayo@gmail.com\n1. Back to Main Menu",
       healthcare: "Healthcare services:\n1. Find a Hospital\n2. Emergency Numbers\n3. Back",
       education: "Education services:\n1. Schools\n2. Universities\n3. Back",
       transportation: "Transportation services:\n1. Public Transport\n2. Taxi Services\n3. Back",
@@ -17,7 +17,7 @@ const translations = {
     rw: {
       welcome: "Murakaza neza muri Serivisi z'u Rwanda\n1. Gushaka Serivisi\n2. Twandikire\n3. Hindura Ururimi ujye mu Cyongereza",
       findServices: "Hitamo serivisi:\n1. Ubuzima\n2. Uburezi\n3. Gutwara Abantu n'Ibintu\n4. Gusubira ku Ibanze",
-      contactUs: "Twandikire:\nTelefone: +250 XXX XXX XXX\nImeyili: info@example.rw\n1. Gusubira ku Ibanze",
+      contactUs: "Twandikire:\nTelefone: 0786729283\nImeyili: abayo@gmail.com\n1. Gusubira ku Ibanze",
       healthcare: "Serivisi z'ubuzima:\n1. Gushaka Ibitaro\n2. Nimero z'Ubutabazi\n3. Gusubira Inyuma",
       education: "Serivisi z'uburezi:\n1. Amashuri\n2. Za Kaminuza\n3. Gusubira Inyuma",
       transportation: "Serivisi zo gutwara abantu n'ibintu:\n1. Gariyamoshi Rusange\n2. Serivisi za Tagisi\n3. Gusubira Inyuma",
